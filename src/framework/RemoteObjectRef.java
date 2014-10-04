a15b1221aa444dc92bc667fdc0772fe2351aedbf
TorinYu <yutaoitper@gmail.com>
2014-10-03 21:02:10 -0400

    Override the equals and hashcode function of RemoteObjectRef

--- src/framework/RemoteObjectRef.java
+++ src/framework/RemoteObjectRef.java
@@ -43,22 +43,22 @@ public class RemoteObjectRef implements Remote, Serializable {
 		this.remoteInterfaceName = riname;
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-	        return false;
-	    }
-	    RemoteObjectRef newObj = (RemoteObjectRef) obj;
-		
-	    if (!this.host.equals(newObj.getHost()) || this.port != newObj.getPort() 
-	    			|| !this.remoteInterfaceName.equals(newObj.getRemoteInterfaceName())) {
-	        return false;
-	    }
-	    return true;
-	}
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (obj == null) {
+//			return false;
+//		}
+//		if (getClass() != obj.getClass()) {
+//	        return false;
+//	    }
+//	    RemoteObjectRef newObj = (RemoteObjectRef) obj;
+//		
+//	    if (!this.host.equals(newObj.getHost()) || this.port != newObj.getPort() 
+//	    			|| !this.remoteInterfaceName.equals(newObj.getRemoteInterfaceName())) {
+//	        return false;
+//	    }
+//	    return true;
+//	}
 
 	/**
 	 * 
@@ -95,5 +95,48 @@ public class RemoteObjectRef implements Remote, Serializable {
 
 	public void setHost(String host) {
 		this.host = host;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((host == null) ? 0 : host.hashCode());
+		result = prime * result + port;
+		result = prime
+				* result
+				+ ((remoteInterfaceName == null) ? 0 : remoteInterfaceName
+						.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RemoteObjectRef other = (RemoteObjectRef) obj;
+		if (host == null) {
+			if (other.host != null)
+				return false;
+		} else if (!host.equals(other.host))
+			return false;
+		if (port != other.port)
+			return false;
+		if (remoteInterfaceName == null) {
+			if (other.remoteInterfaceName != null)
+				return false;
+		} else if (!remoteInterfaceName.equals(other.remoteInterfaceName))
+			return false;
+		return true;
 	}
 }
